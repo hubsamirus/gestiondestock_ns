e4f16c30e4e3549e7fb00b7f4f1e160d2e35bb65
nait said samir <naitsaid_2002@yahoo.fr>
2017-11-12 11:25:50 -0500

    valider

--- app/Http/Controllers/AchatController.php
+++ app/Http/Controllers/AchatController.php
@@ -18,15 +18,7 @@ class AchatController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
